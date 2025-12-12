d0957f7d2b308c762233d797b026e0070cd6f114
Celes Renata <celes@celestium.life>
2025-12-12 15:33:52 -0800

    Uncomment quickshell in package list

--- packages/dots-hyprland-packages.nix
+++ packages/dots-hyprland-packages.nix
@@ -32,7 +32,7 @@ let
     hyprlock
     hyprpicker
     networkmanagerapplet # nm-connection-editor
-    # quickshell - User must add to home.packages from nixpkgs 25.11+
+    quickshell
     translate-shell
     wlogout
     
